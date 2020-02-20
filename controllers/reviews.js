02d2dfb121391271ee0c50b46b75dc00746feab8
CoderGuy614 <jlutz.110@gmail.com>
2020-02-20 16:23:09 +0800

    References - Reviews - Finished and Working

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,8 +1,12 @@
-const Reviews = require('../models/reviews')
-const router = require('express').Router()
+const Reviews = require("../models/reviews");
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-	// Respond with all reviews belonging to specific house and populate nested fields
-})
+router.get(`/`, (req, res) => {
+  console.log(req.query);
+  Reviews.find(req.query)
+    .populate("author", "houses")
+    .then(r => res.send(r));
+  // Respond with all reviews belonging to specific house and populate nested fields
+});
 
-module.exports = router
+module.exports = router;
--- models/reviews.js
+++ models/reviews.js
@@ -1,21 +1,21 @@
 const mongoose = require("mongoose");
 const objectId = mongoose.Schema.Types.ObjectId;
-
+const users = require("../models/users");
 module.exports = mongoose.model("reviews", {
-  reviews: [
-    {
-      author: {
-        type: objectId,
-        ref: "users"
-      },
-      rating: {
-        type: Number,
-        default: 0
-      },
-      content: {
-        type: String,
-        required: [true, "content is required"]
-      }
-    }
-  ]
+  author: {
+    type: objectId,
+    ref: "users"
+  },
+  rating: {
+    type: Number,
+    default: 0
+  },
+  content: {
+    type: String,
+    required: [true, "content is required"]
+  },
+  house: {
+    type: objectId,
+    ref: "houses"
+  }
 });
