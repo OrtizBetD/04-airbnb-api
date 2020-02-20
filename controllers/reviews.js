8d3c650da9cb4875a9ad917404d05b1e3c292104
CoderGuy614 <jlutz.110@gmail.com>
2020-02-20 16:31:56 +0800

    portal step - references - finished

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -2,11 +2,9 @@ const Reviews = require("../models/reviews");
 const router = require("express").Router();
 
 router.get(`/`, (req, res) => {
-  console.log(req.query);
   Reviews.find(req.query)
     .populate("author", "houses")
     .then(r => res.send(r));
-  // Respond with all reviews belonging to specific house and populate nested fields
 });
 
 module.exports = router;
